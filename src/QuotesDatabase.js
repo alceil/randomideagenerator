e1d458a82e62554e255cd6c485301b3742197024
alceil <ashishshajisrampickal@gmail.com>
2021-06-27 13:37:19 +0530

    first commit

---
+++ src/QuotesDatabase.js
@@ -0,0 +1,19 @@
+const quotes=[
+	{
+    "quote": " Social Media Dashboard",
+    "author": "MERN"
+    }, {
+    "quote": "Dating App Manager",
+    "author": "MERN"
+    }, {
+    "quote": "Computer Game Companion App",
+    "author": "MERN"
+    }, {
+    "quote": " Job Search App.",
+    "author": "MERN"
+    }, {
+    "quote": "100DaysOfCode App",
+    "author": "MERN"
+    }
+]
+export default quotes
